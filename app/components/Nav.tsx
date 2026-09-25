e038dfdb57f135623ce3a7614181712fa66a484c
dh1-Omarchy <237053497+Samz6293@users.noreply.github.com>
2026-09-25 19:29:48 +0600

    Fix Nav links inconsistencies

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,6 +8,7 @@ import { RiHome2Line } from "react-icons/ri";
 import logo from "@/app/assets/logo.png"
 import Image from "next/image";
 import { ExerciseContextCreate } from "../context/ExerciseContext";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
 
@@ -21,6 +22,7 @@ const Nav = () => {
             document.body.style.overflow = "auto";
         }
     },[isOpen])
+    const pathname = usePathname();
 
     const {onLink, setOnLink, plan, saved} = useContext(ExerciseContextCreate);
     const handleOnLink = (link: string) => {
@@ -60,7 +62,7 @@ const Nav = () => {
                 <ul className="hidden justify-center items-center text-gray-500 gap-4 text-xs
                 md:flex">
                     {navlinks.map((link) => (
-                        <li key={link.name} className={`hover:text-lime-400 ${link.name === onLink ? "font-bold text-lime-400 rounded-full px-3 py-1 bg-lime-800/20" : ""}`}>
+                        <li key={link.name} className={`hover:text-lime-400 ${link.page === pathname && onLink !== "none" ? "font-bold text-lime-400 rounded-full px-3 py-1 bg-lime-800/20" : ""}`}>
                             <Link onClick={() => handleOnLink(link.name)} href={link.page}>
                                 {link.name}
                             </Link>
