5a8e166525451ac234d290f41a123d6e665629f4
dh1-Omarchy <237053497+Samz6293@users.noreply.github.com>
2026-09-26 09:34:31 +0600

    Fix responsive ui issues

--- app/exercise/[id]/page.tsx
+++ app/exercise/[id]/page.tsx
@@ -49,28 +49,28 @@ const ExerciseDetailPage = async({params}: ExerciseDeatailsParams) => {
 
                         <div className="end-to-end text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Difficulty</h4>
-                            <p className="text-sm text-end">{exerciseDetails.difficulty}</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.difficulty}</p>
                         </div>
                         <div className="end-to-end text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Sets</h4>
-                            <p className="text-sm text-end">{exerciseDetails.sets}</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.sets}</p>
                         </div>
                         <div className="end-to-end text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Reps</h4>
-                            <p className="text-sm text-end">{exerciseDetails.reps}</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.reps}</p>
                         </div>
                         <div className="end-to-end text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Duration</h4>
-                            <p className="text-sm text-end">{exerciseDetails.duration} min</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.duration} min</p>
                         </div>
                         <div className="end-to-end text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Calories</h4>
-                            <p className="text-sm text-end">{exerciseDetails.caloriesBurned} kcal</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.caloriesBurned} kcal</p>
                         </div>
 
                         <div className="end-to-end border-none text-xs px-6 py-3">
                             <h4 className="font-bold text-start text-gray-400">Rating</h4>
-                            <p className="text-sm text-end">{exerciseDetails.rating}</p>
+                            <p className="text-sm text-start md:text-end">{exerciseDetails.rating}</p>
                         </div>
                     </div>
 
